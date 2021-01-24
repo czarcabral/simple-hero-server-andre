d9ff7482b605361c851ed25e65d45d14cda42c76
Gabriel Cabral <czar.cabral@gmail.com>
2021-01-23 22:22:09 -0800

    test out post request form data

--- main.go
+++ main.go
@@ -42,7 +42,8 @@ func handleAllHeroesRequest(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
-
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	switch r.Method {
 	case "GET" :
 		lastHeroIds, ok := r.URL.Query()["lastHeroId"]
