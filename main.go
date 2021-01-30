e32ad3064bcab8ed04d59963b9fd364715222891
Gabriel Cabral <czar.cabral@gmail.com>
2021-01-29 17:00:52 -0800

    add cors options

--- main.go
+++ main.go
@@ -233,6 +233,7 @@ func handleHeroesIDRoute(heroes *[]Hero) func(http.ResponseWriter, *http.Request
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 		// extract hero id from url path variable
 		vars := mux.Vars(r)
